b0ddc4e6a6621fb1db479ce6d420e5d9b8324569
T2004-la <Tara.la2004@gmail.com>
2026-08-16 21:01:51 +0330

    Add Part 4: bloglist application

--- part2/phonebook/src/services/persons.js
+++ part2/phonebook/src/services/persons.js
@@ -1,6 +1,6 @@
  // src/services/persons.js
 import axios from 'axios'
-const baseUrl = 'http://localhost:3001/persons'
+const baseUrl = 'http://localhost:3001/api/persons'
 
 const getAll = () => {
   const request = axios.get(baseUrl)
